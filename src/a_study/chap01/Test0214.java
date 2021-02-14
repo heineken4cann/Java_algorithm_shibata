b95e698f1c51f54ee093a969775d3c77fdf71410
heineken4cann1 <heineken4can@gmail.com>
2021-02-14 17:45:58 +0900

    heineken4cann 1 commit test

---
+++ src/a_study/chap01/Test0214.java
@@ -0,0 +1,10 @@
+package a_study.chap01;
+
+public class Test0214 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test 0214 2======>");
+	}
+
+}
